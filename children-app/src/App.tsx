1573ce18cfc4f6704c1a80802e9c834d3f109e3b
Trung Tran <tranhuutrung1408.work@gmail.com>
2023-11-10 22:24:49 +0700

    update children form

--- children-app/src/App.tsx
+++ children-app/src/App.tsx
@@ -9,8 +9,24 @@ export default function App() {
   const [form] = Form.useForm();
 
   const onFinish = (values: any) => {
-    const { firstName, lastName } = values;
-    sendMessageToParent({ firstName, lastName });
+    const { firstName, lastName, address } = values;
+    sendMessageToParent({
+      action: 'SUBMIT',
+      data: {
+        firstName: {
+          value: firstName,
+          type: 'String',
+        },
+        lastName: {
+          value: lastName,
+          type: 'String',
+        },
+        address: {
+          value: address,
+          type: 'String',
+        },
+      },
+    });
   };
 
   useEffect(() => {
@@ -19,42 +35,39 @@ export default function App() {
         console.log('[Children] Received message from parent app:', event.data);
         const { firstName, lastName } = event.data;
         form.setFieldsValue({ firstName, lastName });
-  
+
         // Send a response back to the parent
         if (event.source) {
-          event.source.postMessage({ message: 'Children already received data' }, MAIN_DOP_HOST as any);
+          event.source.postMessage(
+            { message: 'Children already received data' },
+            MAIN_DOP_HOST as any
+          );
         }
       }
     });
   }, []);
-  
+
   const sendMessageToParent = useCallback((message: unknown) => {
     console.log('[CHILD] sendMessageToParent', message);
     window.parent.postMessage(message, MAIN_DOP_HOST);
   }, []);
 
   return (
-    <Row gutter={16}>
-      <Col span={20} offset={2}>
-        <Title level={4}>[Children] Building sample form</Title>
-        <Form
-          layout="vertical"
-          form={form}
-          onFinish={onFinish}
-        >
-          <Form.Item label="First name" name="firstName">
-            <Input placeholder="input first name" />
-          </Form.Item>
-          <Form.Item label="Last name" name="lastName">
-            <Input placeholder="input last name" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </Col>
-    </Row>
+    <Form layout="vertical" form={form} onFinish={onFinish}>
+      <Form.Item label="First name" name="firstName">
+        <Input placeholder="input first name" />
+      </Form.Item>
+      <Form.Item label="Last name" name="lastName">
+        <Input placeholder="input last name" />
+      </Form.Item>
+      <Form.Item label="Address" name="address">
+        <Input placeholder="input address" />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
